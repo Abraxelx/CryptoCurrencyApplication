98543b32c0aaa2af96eda70c600e5d7625a7f869
xelil <hsahin@skylandsoft.com>
2022-08-09 15:31:30 +0300

    removed realtime Db - added firestore

--- app/src/main/java/com/abraxel/cryptocurrency/RemindMeActivity.java
+++ app/src/main/java/com/abraxel/cryptocurrency/RemindMeActivity.java
@@ -1,6 +1,7 @@
 package com.abraxel.cryptocurrency;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,8 +14,8 @@ import android.widget.Toast;
 import com.abraxel.cryptocurrency.constants.Constants;
 import com.abraxel.cryptocurrency.model.DbData;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Locale;
 
@@ -25,10 +26,10 @@ public class RemindMeActivity extends AppCompatActivity {
     private Button setReminderButton;
     private ElegantNumberButton elegantNumberButton;
     private ImageView imageViewReminder;
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference db;
     String coinNameFromBundle;
     double coinValueFromBundle;
+
+    private FirebaseFirestore firebaseDatabase;
 
 
     @Override
@@ -58,17 +59,16 @@ public class RemindMeActivity extends AppCompatActivity {
         setReminderButton.setOnClickListener(view -> {
             if (valueSetter.getText() != null) {
                 DbData dbData = new DbData();
-                firebaseDatabase = FirebaseDatabase.getInstance(Constants.FIREBASE_URL);
-                db = firebaseDatabase.getReference(coinNameFromBundle.toLowerCase(Locale.ROOT) + "-reminder");
+                firebaseDatabase = FirebaseFirestore.getInstance();
                 dbData.setCoinName(coinNameFromBundle);
                 dbData.setCoinVal(Integer.valueOf(valueSetter.getText().toString()));
-                db.setValue(dbData, (error, ref) -> {
-                    if (error != null) {
-                        Toast.makeText(getApplicationContext(), getString(R.string.data_could_not_be_saved) + error.getMessage(), Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), getString(R.string.reminder_set_successful), Toast.LENGTH_LONG).show();
-                    }
-                });
+                firebaseDatabase.collection("currencies").document(coinNameFromBundle.toLowerCase(Locale.ROOT) + "-reminder").set(dbData)
+                        .addOnSuccessListener(unused -> Toast.makeText(getApplicationContext(), getString(R.string.reminder_set_successful), Toast.LENGTH_LONG).show()).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception error) {
+                                Toast.makeText(getApplicationContext(), getString(R.string.data_could_not_be_saved) + error.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
 
